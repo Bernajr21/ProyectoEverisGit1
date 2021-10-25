d27a11ad8b4e0a45e9a4daf6cab6eeb727a40a37
bernajr21 <bernajr1010@gmail.com>
2021-10-25 14:17:16 +0200

    primer commit

---
+++ src/main/java/com/everis/everisgit/App.java
@@ -0,0 +1,13 @@
+package com.everis.everisgit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
